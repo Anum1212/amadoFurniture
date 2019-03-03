5a01086d6e1074df3d5a638e633b0d3005e33fe2
Anum1212 <anamamer0@gmail.com>
2019-03-04 02:32:12 +0500

    new backup

--- DB BackUp/furniture.sql
+++ DB BackUp/furniture.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2018 at 07:33 AM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
+-- Generation Time: Mar 03, 2019 at 10:13 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -118,8 +118,24 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `category_id`, `brand`, `brand_soundex`, `name`, `name_soundex`, `description`, `price`, `quantity`, `product_status`) VALUES
-(1, 6, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1),
-(2, 6, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1);
+(1, 1, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1),
+(2, 1, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1),
+(3, 2, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1),
+(4, 2, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1),
+(5, 3, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1),
+(6, 3, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1),
+(7, 4, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1),
+(8, 4, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1),
+(9, 5, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1),
+(10, 5, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1),
+(11, 6, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1),
+(12, 6, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1),
+(14, 7, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1),
+(15, 7, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1),
+(16, 8, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1),
+(17, 8, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1),
+(18, 9, 'ikea', 'I200', 'flower pot', 'F460 P300', 'flower pot', 8200, 46, 1),
+(19, 9, 'ikea', 'I200', 'luxury sofa', 'L260 S100', 'luxury sofa', 82000, 46, 1);
 
 -- --------------------------------------------------------
 
@@ -146,7 +162,71 @@ INSERT INTO `product_images` (`id`, `product_id`, `image_name`, `image_number`) 
 (5, 2, 'prd2_01.jpg', 0),
 (6, 2, 'prd2_02.jpg', 1),
 (7, 2, 'prd2_03.jpg', 2),
-(8, 2, 'prd2_04.jpg', 3);
+(8, 2, 'prd2_04.jpg', 3),
+(9, 3, 'prd1_01.jpeg', 0),
+(10, 3, 'prd1_02.jpg', 1),
+(11, 3, 'prd1_03.jpg', 2),
+(12, 3, 'prd1_04.jpg', 3),
+(13, 4, 'prd2_01.jpg', 0),
+(14, 4, 'prd2_02.jpg', 1),
+(15, 4, 'prd2_03.jpg', 2),
+(16, 4, 'prd2_04.jpg', 3),
+(17, 5, 'prd1_01.jpeg', 0),
+(18, 5, 'prd1_02.jpg', 1),
+(19, 5, 'prd1_03.jpg', 2),
+(20, 5, 'prd1_04.jpg', 3),
+(21, 6, 'prd2_01.jpg', 0),
+(22, 6, 'prd2_02.jpg', 1),
+(23, 6, 'prd2_03.jpg', 2),
+(24, 6, 'prd2_04.jpg', 3),
+(25, 7, 'prd1_01.jpeg', 0),
+(26, 7, 'prd1_02.jpg', 1),
+(27, 7, 'prd1_03.jpg', 2),
+(28, 7, 'prd1_04.jpg', 3),
+(29, 8, 'prd2_01.jpg', 0),
+(30, 8, 'prd2_02.jpg', 1),
+(31, 8, 'prd2_03.jpg', 2),
+(32, 8, 'prd2_04.jpg', 3),
+(33, 9, 'prd2_01.jpg', 0),
+(34, 9, 'prd2_02.jpg', 1),
+(35, 9, 'prd2_03.jpg', 2),
+(36, 9, 'prd2_04.jpg', 3),
+(37, 10, 'prd1_01.jpeg', 0),
+(38, 10, 'prd1_02.jpg', 1),
+(39, 10, 'prd1_03.jpg', 2),
+(40, 10, 'prd1_04.jpg', 3),
+(41, 11, 'prd1_01.jpeg', 0),
+(42, 11, 'prd1_02.jpg', 1),
+(43, 11, 'prd1_03.jpg', 2),
+(44, 11, 'prd1_04.jpg', 3),
+(45, 12, 'prd1_01.jpeg', 0),
+(46, 12, 'prd1_02.jpg', 1),
+(47, 12, 'prd1_03.jpg', 2),
+(48, 12, 'prd1_04.jpg', 3),
+(49, 14, 'prd1_01.jpeg', 0),
+(50, 14, 'prd1_02.jpg', 1),
+(51, 14, 'prd1_03.jpg', 2),
+(52, 14, 'prd1_04.jpg', 3),
+(53, 15, 'prd2_01.jpg', 0),
+(54, 15, 'prd2_02.jpg', 1),
+(55, 15, 'prd2_03.jpg', 2),
+(56, 15, 'prd2_04.jpg', 3),
+(57, 16, 'prd1_01.jpeg', 0),
+(58, 16, 'prd1_02.jpg', 1),
+(59, 16, 'prd1_03.jpg', 2),
+(60, 16, 'prd1_04.jpg', 3),
+(61, 17, 'prd1_01.jpeg', 0),
+(62, 17, 'prd1_02.jpg', 1),
+(63, 17, 'prd1_03.jpg', 2),
+(64, 17, 'prd1_04.jpg', 3),
+(65, 18, 'prd1_01.jpeg', 0),
+(66, 18, 'prd1_02.jpg', 1),
+(67, 18, 'prd1_03.jpg', 2),
+(68, 18, 'prd1_04.jpg', 3),
+(69, 19, 'prd1_01.jpeg', 0),
+(70, 19, 'prd1_02.jpg', 1),
+(71, 19, 'prd1_03.jpg', 2),
+(72, 19, 'prd1_04.jpg', 3);
 
 -- --------------------------------------------------------
 
@@ -252,13 +332,13 @@ ALTER TABLE `order_items`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT for table `users`
